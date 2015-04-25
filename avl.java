dfd0db425213608d681abe46011ccba4301270ac
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-04-25 11:20:15 +0100

    Basic methods for AVL Trees

---
+++ avl.java
@@ -0,0 +1,70 @@
+// Rotates the left tree right, bringing the left child of the 
+// current node to the root of this tree section
+public TreeNode<K, V> rotateRight(TreeNode<K, V> node) {
+	TreeNode<K, V> left = node.getLeft();
+	node.setLeft(left.getRight());
+	left.setRight(node);
+	return left;
+}
+
+// Rotates the right tree left, bringing the right child of the 
+// current node to the root of this tree section
+public TreeNode<K, V> rotateLeft(TreeNode<K, V> node) {
+	TreeNode<K, V> right = node.getRight();
+	node.setRight(right.getLeft());
+	right.setLeft(node);
+	return right;
+}
+
+// Rotates the left tree left before rotating the root right
+public TreeNode<K, V> rotateLeftRight(TreeNode<K, V> node) {
+	TreeNode<K, V> newLeft = rotateLeft(node.getLeft());
+	node.setLeft(newLeft);
+	return rotateRight(node)
+}
+
+// Rotates the right tree right before rotating the root left
+public TreeNode<K, V> rotateRightLeft(TreeNode<K, V> node) {
+	TreeNode<K, V> newRight = rotateRight(node.getRight());
+	node.setRight(newRight);
+	return rotateLeft(node)
+}
+
+// Rebalances the node from the top
+public TreeNode<K, V> rebalance(TreeNode<K, V> node) {
+	heightdiff = getHeightDifference(node);
+	if (heightdiff > 1) {
+		heightdiff = getHeightDifference(node.getLeft());
+		if (heightdiff > 0) {
+			rotateRight(node);
+		} else {
+			rotateLeftRight(node);
+		}
+	} else if (heightdiff < -1) {
+		heightdiff = getHeightDifference(node.getRight());
+		if (heightdiff < 0) {
+			rotateLeft(node);
+		} else {
+			rotateRightLeft(node);
+		}
+	}
+	return node
+}
+
+// Gets the height difference between the two children of a node
+// Returning 0 if they are the same or the node is null
+// Returning >= 1 if the left is greater than the right
+// Returning <= -1 if the right is greater than the left
+private int getHeightDifference(TreeNode<K, V> node) {
+	return node != null ?
+		getHeight(node.getLeft()) - getHeight(node.getRight())
+		: 0;
+}
+
+// If the node is null, returns 0
+// Otherwise returns the height of each child + 1
+private int getHeight(TreeNode<K, V> node) {
+	return node != null ? 
+		1 + getHeight(node.getLeft()) + getHeight(node.getRight())
+		: 0;
+}
