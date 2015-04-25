519bb87e59b84ce16090c8227ed06619d2353022
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-04-25 12:13:10 +0100

    Added RBT, only description

---
+++ rbt.java
@@ -0,0 +1,12 @@
+// Red black trees
+// This is a binary search tree, whose nodes have the colour *Red* or *Black*
+//
+// - Every node is either red or black
+// - The root node is black
+// - If a node is red, it's children are all black
+// - Every path from a given node to any of it's descendants contains
+//   the same number of black nodes.
+
+// Height h <= 2 log (n+1) where n is number of elements
+
+
